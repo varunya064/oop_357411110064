b8f2de38dfc6096f19bb8b11e8484faf371bcf4c
varunya064 <kwannajak@gmail.com>
2017-11-19 14:49:03 +0700

    Testing upload to github

---
+++ src/oop_lap/s357411110064/rmutsv/com/Helloworld.java
@@ -0,0 +1,13 @@
+package oop_lap.s357411110064.rmutsv.com;
+
+public class Helloworld {
+//    create main_method
+public static void main(String[] args) {
+   System.out.println("Hello World, i'am Kwan");
+
+
+
+}//main
+
+
+}//class
