73866ad1c34ee992118b837c22c5184459fe9fd1
varunya sudjan <kwannajak@gmail.com>
2017-12-12 10:26:51 +0700

    test forloop

---
+++ src/OOP_Lab3/TestForLoop.java
@@ -0,0 +1,31 @@
+package OOP_Lab3;
+
+public class TestForLoop {
+    public static void main(String[] args) {
+       for (int i=1;i<=10;i++)
+           System.out.println("Hello");
+
+        for (int i=1;i<=10;i++)
+        System.out.println(i);
+
+     for (int i=1;i<=100;i++)
+            System.out.print(i);
+
+        for (int i=1;i<=100;i++)
+           System.out.println(i);
+
+        for (int i=1;i<=100;i++){
+            if (i%2==0)
+                System.out.println(i+"");
+            }
+        for (int i=1;i<=100;i++){
+            if (i%3==0 && i%5==0)
+                System.out.println(i+"");
+        }
+
+
+
+
+    }//main
+
+}//class
