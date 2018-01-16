21c4c91e4e62de6310760e666317d059e57c7389
varunya <kwannajak@gmail.com>
2018-01-16 10:58:37 +0700

    StringToken

--- src/OOP_LAB6/StringToken.java
+++ src/OOP_LAB6/StringToken.java
@@ -1,16 +1,19 @@
 package OOP_LAB6;
+import java.util.StringTokenizer;
 
 public class StringToken {
     public static void main(String[] args) {
-
-        String msg = "Hello, RMUTSV varunya";
+        String msg = "Hello, RMUTSV Varunya Sudjan";
         StringTokenizer myToken = new StringTokenizer(msg);
         System.out.println(myToken.countTokens());
 
         while (myToken.hasMoreTokens()){
-            StringBuffer myBuf = new StringBuffer(myToken.nextToken());
-            System.out.println(myBuf.reverse()+"  ");
+            StringBuffer myBuf = new StringBuffer(
+                    myToken.nextToken());
+            System.out.print(myBuf.reverse()+" ");
         }//while
+
+
 
 
     }//main
