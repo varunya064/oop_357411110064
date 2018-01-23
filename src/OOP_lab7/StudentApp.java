28e76ef87785e16cab79437c987abeef3ae89df8
varunya sudjan <kwannajak@gmaul.com>
2018-01-23 09:19:19 +0700

    midterm3.3

---
+++ src/OOP_lab7/StudentApp.java
@@ -0,0 +1,14 @@
+package OOP_lab7;
+
+public class StudentApp {
+
+    public static void main(String[] args) {
+      //create objects as Student
+        Student student1 = new Student();
+
+
+
+
+
+    }//main
+}//class
