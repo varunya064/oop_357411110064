ba4acc916e21f76a3c0196645b33537f5315b33c
varunya <kwannajak@gmail.com>
2018-01-30 09:59:29 +0700

    PersonInfo

---
+++ src/OOP_Lab8/PersonInfo.java
@@ -0,0 +1,18 @@
+package OOP_Lab8;
+
+public class PersonInfo {
+    public static void main(String[] args) {
+        Student student = new Student("1801700071182",
+                "kwan",
+                "male",
+                20,
+                "357411110064",
+                "IS");
+
+        System.out.println(student.toString());
+        System.out.println (student.getName());
+    }
+
+
+
+}
